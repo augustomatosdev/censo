93992f71fe234d7655ed45a9c12dd6a6634eaff9
augustomatosdev <dmatosconsult@gmail.com>
2024-10-05 13:36:14 +0100

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,20 @@
-"use client";
-import Link from "next/link";
-import { updateFirestoreWithAdditionalFields } from "@/lib/uploadfb/addToFirebase";
+import React from "react";
+import { SelectAs } from "./search-as/select-as";
 
-export default function Home() {
-  const municipios = [
-    "porto_amboim",
-    "sumbe",
-    "seles",
-    "conda",
-    "amboim",
-    "quilenda",
-    "libolo",
-    "mussende",
-    "quibala",
-    "ebo",
-    "cela",
-    "cassongue",
-  ];
-
+const Page = async () => {
   return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold text-center">
-        SELECCIONE O SEU MUNICIPIO
-      </h1>
-      <div>
-        {municipios.map((el, key) => (
-          <Link
-            key={key}
-            href={`/${el}`}
-            className={`m-8 inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75`}
-          >
-            {el}
-          </Link>
-        ))}
-      </div>
-      <div>
-        <button onClick={() => updateFirestoreWithAdditionalFields()}>
-          addData
-        </button>
+    <div className="max-w-screen-md mx-auto">
+      <div className="p-4 py-8">
+        <p className="text-center text-2xl font-bold">ÁREA DE SUPERVISÃO</p>
+        <p className="text-center my-6">
+          Por favor seleccione a sua área de supervisão para actualizar os dados
+        </p>
+        <div>
+          <SelectAs />
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default Page;
